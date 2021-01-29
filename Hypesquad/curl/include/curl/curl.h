a18078679d08c15e258255cff3023d9e30e6626e
rond <rkulleseid@gmail.com>
2021-01-29 11:38:34 +0100

    Refactor curl.h
    codefactor was mad so I fixed a line

--- Hypesquad/curl/include/curl/curl.h
+++ Hypesquad/curl/include/curl/curl.h
@@ -2865,7 +2865,6 @@ struct curl_version_info_data {
   unsigned int zstd_ver_num; /* Numeric Zstd version
                                   (MAJOR << 24) | (MINOR << 12) | PATCH */
   const char *zstd_version; /* human readable string. */
-
 };
 typedef struct curl_version_info_data curl_version_info_data;
 
